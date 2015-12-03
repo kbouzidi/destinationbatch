b1495cdbe081ef2e838ce4b658fa0187c212e6ad
kbouzidi <kr.bouzidi@gmail.com>
2015-12-03 13:24:40 +0100

    Add Promise Fix server and async issues

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var express = require('express'),
     batchArgu = process.argv.slice(2),
     taxonomieFile = batchArgu[0],
     destinationFile = batchArgu[1],
-    parser = require('./lib/parser');
+    parse = require('./lib/parse');
 
 var server_port = config.port || 1405;
 var server_ip_address = config.host || '127.0.0.1';
@@ -23,8 +23,15 @@ var taxonomieStream = fs.createReadStream(taxonomieFile);
 var destinationStream = fs.createReadStream(destinationFile);
 
 var taxonomiesData, destinationsData;
+parse.parseTaxonomies(taxonomieStream).then(function (res) {
+    taxonomiesData = res;
+    console.log(taxonomiesData);
+});
 
-
+parse.parseDestination(destinationStream).then(function (res) {
+    taxonomieStream = res;
+    console.log(taxonomieStream);
+});
 require('./common/config_app')(app, config);
 logger.info('STARTING THE DESTINATION SERVER');
 logger.info('-------------------------');
@@ -35,7 +42,8 @@ app.listen(server_port, server_ip_address, function () {
 logger.info('Started the server');
 
 
-//taxonomiesData = this.parser.parseTaxonomies(taxonomieStream);
-//destinationsData = parser.parseDestination(destinationStream);
+
+destinationsData = parse.parseDestination(destinationStream);
+
 
 

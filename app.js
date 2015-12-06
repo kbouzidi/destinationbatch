86a50a88660309bfe795845d4c835e3dededf205
kb00 <kr.bouzidi@gmail.com>
2015-12-06 20:34:42 +0100

    Fix typo

--- app.js
+++ app.js
@@ -8,22 +8,22 @@
 var log4js = require('log4js'),
     logger = log4js.getLogger('APP'),
     fs = require('fs'),
-    batchArgu = process.argv.slice(2),
-    taxonomieFile = batchArgu[0],
-    destinationFile = batchArgu[1],
+    batchArg = process.argv.slice(2),
+    taxonomyFile = batchArg[0],
+    destinationFile = batchArg[1],
     parser = require('./lib/parser'),
     q = require('./lib/utils/q'),
     gen = require('./lib/documentgenerator');
 
 
-var taxonomieStream = fs.createReadStream(taxonomieFile);
+var taxonomyStream = fs.createReadStream(taxonomyFile);
 var destinationStream = fs.createReadStream(destinationFile);
 
 
-var outputDestination = batchArgu[2];
+var outputDestination = batchArg[2];
 var templateSource = __dirname + '/template/';
 
-q.all([parser.parseTaxonomies(taxonomieStream), parser.parseDestination(destinationStream)])
+q.all([parser.parseTaxonomies(taxonomyStream), parser.parseDestination(destinationStream)])
     .spread(function (taxonomies, destination) {
         return gen.generateHtmls(taxonomies, destination, outputDestination, templateSource);
     }).then(function (res) {

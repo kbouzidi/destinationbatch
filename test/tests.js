2b5aedf766d178d33c66a3288fc62f745b769462
kb00 <kr.bouzidi@gmail.com>
2015-12-06 18:56:40 +0100

    Fix test

--- test/tests.js
+++ test/tests.js
@@ -13,7 +13,7 @@ var path = require('path');
 
 var destinationsJson, taxonomieJson;
 
-describe('Test parser', function () {
+describe('Test Project', function () {
 
 
     it('Test taxonomies parse', function (done) {
@@ -22,7 +22,7 @@ describe('Test parser', function () {
         parser.parseTaxonomies(taxonomieStream).then(function (result) {
             assert.isNotNull(result);
             taxonomieJson = result;
-            json2file.writeFile(__dirname + '/testOutput/taxonomies.json', result, function (err) {
+            json2file.writeFile(__dirname + '/testdata/output/taxonomies.json', result, function (err) {
                 if (err) {
                     console.log(err);
                     assert.isNull(err, 'there was no error');
@@ -39,7 +39,7 @@ describe('Test parser', function () {
         parser.parseDestination(destinations).then(function (result) {
             assert.isNotNull(result);
             destinationsJson = result;
-            json2file.writeFile(__dirname + '/testOutput/destinations.json', result[0], function (err) {
+            json2file.writeFile(__dirname + '/testdata/output/destinations.json', result[0], function (err) {
                 if (err) {
                     assert.isNull(err, 'there was no error');
                 }
@@ -68,7 +68,7 @@ describe('Test parser', function () {
             fse.removeSync(__dirname + '/testdata/output/' + '.DS_Store');
         }
         var files = fs.readdirSync(__dirname + '/testdata/output/');
-        assert.equal(destinationsJson.length, files.length);
+        assert.equal(destinationsJson.length + 3, files.length);
         done();
 
 

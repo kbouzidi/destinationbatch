a1f4f6a76152cb96765f849ea69363adf00e115f
kb00 <kr.bouzidi@gmail.com>
2015-12-06 14:31:17 +0100

    Add taxonomies

--- template/js/main.js
+++ template/js/main.js
@@ -16,189 +16,197 @@ angular.module('destinationApp', ['ngMaterial', 'hm.readmore'])
 
         console.log("START");
         $http.get('data/destination.json').success(function (data) {
-            $scope.name = data.name;
-            $scope.id = data.id;
+            $http.get('data/taxonomies.json').success(function (taxonomies) {
+                console.log("taxonomies");
 
-            if (data.history || data.overview || data.introductory) {
-                $scope.home = true;
-                $scope.homeMenu = true;
-                if (_.get(data, 'history', null)) {
-                    $scope.histories = data.history;
-                    $scope.history = '';
-                    for (var i = 0; i < data.history.length; i++) {
-                        $scope.history += '\n' + data.history[i];
-
-                    }
-
-                    if (data.overview) {
-                        $scope.history += '\n' + _.get(data, 'overview', null);
-                    }
-
-                    if (data.overview) {
-                        $scope.history += '\n' + _.get(data, 'introductory.introduction.overview', null)
-                    }
-
-                } else if (data.overview) {
-
-                    $scope.history += '\n' + data.overview
-                } else if (data.introductory) {
-
-                    $scope.history += '\n' + _.get(data, 'introductory.introduction.overview', null);
-                }
-
-            }
-
-
-            var result = [];
-            for (var key in data) {
-                if (key == 'practical_information') {
-
-                    $scope.before_you_go = _.get(data, 'practical_information.health_and_safety.before_you_go', null);
-                    $scope.in_transit = _.get(data, 'practical_information.health_and_safety.in_transit', null);
-                    $scope.dangers_and_annoyances = _.get(data, 'practical_information.health_and_safety.dangers_and_annoyances', null);
-                    $scope.visas = _.get(data, 'practical_information.visas', null);
-                    $scope.while_youre_there = _.get(data, 'practical_information.health_and_safety.while_youre_there', null);
-
-
-                    $scope.cost = _.get(data, 'practical_information.money_and_costs.cost', null);
-                    $scope.money = _.get(data, 'practical_information.money_and_costs.money', null);
-                    if ($scope.cost || $scope.money) {
-                        $scope.money_and_costsShow = true;
-
-                    }
-                    if ($scope.before_you_go ||
-                        data.dangers_and_annoyances ||
-                        data.visas ||
-                        data.while_youre_there ||
-                        data.cost ||
-                        data.money) {
-                        $scope.practical_informationShow = true;
-                        $scope.practical_informationMenu = true;
-                    }
-
+                if (taxonomies) {
+                    $scope.cities = _.pluck(_.reject(taxonomies, { 'id': '355064' }), 'name');
 
                 }
+                $scope.name = data.name;
+                $scope.id = data.id;
 
-
-                if (key == 'transport') {
-
-                    $scope.overviewTransport = _.get(data, 'transport.getting_around.overview', null);
-                    $scope.air = _.get(data, 'transport.getting_around.air', null);
-                    $scope.bicycle = _.get(data, 'transport.getting_around.bicycle', null);
-                    $scope.car_and_motorcycle = _.get(data, 'transport.getting_around.car_and_motorcycle', null);
-                    $scope.local_transport = _.get(data, 'transport.getting_around.local_transport', null);
-                    $scope.train = _.get(data, 'transport.getting_around.train', null);
-                    $scope.gettingThere = _.get(data, 'transport.getting_there_and_away.air', null);
-
-                    if ($scope.overviewTransport ||
-                        $scope.air ||
-                        $scope.bicycle ||
-                        $scope.car_and_motorcycle ||
-                        $scope.local_transport ||
-                        $scope.train ||
-                        $scope.gettingThere) {
-
-                        $scope.transportShow = true;
-                        $scope.transportMenu = true;
-                    }
-                }
-
-
-                if (key == 'weather') {
-                    if (data.weather.when_to_go) {
-                        $scope.overviewWeather = _.get(data, 'weather.when_to_go.overview', null);
-                        $scope.climate = _.get(data, 'weather.when_to_go.climate', null);
-                        if ($scope.overviewWeather ||
-                            $scope.climate) {
-                            $scope.when_to_goShow = true;
-                            $scope.when_to_goMenu = true;
-
-                        }
-                    }
-
-
-                }
-                if (key == 'work_live_study') {
-                    if (data.work_live_study.work) {
-
-                        $scope.overviewWork = _.get(data, 'work_live_study.work.overview', null);
-                        $scope.business = _.get(data, 'work_live_study.work.business', null);
-
-                        if ($scope.overviewWork ||
-                            $scope.business) {
-                            $scope.workShow = true;
-                            $scope.workMenu = true;
-
-                        }
-
-                    }
-
-
-                }
-            }
-
-
-            $scope.keys = result;
-
-
-            $scope.limit = 900;
-            $scope.lessText = "Read less";
-            $scope.moreText = "Read more";
-            $scope.dotsClass = "toggle-dots-grey";
-            $scope.linkClass = "toggle-link-yellow";
-
-            var getSectionOff = function () {
-                $scope.home = false;
-                $scope.workShow = false;
-                $scope.when_to_goShow = false;
-                $scope.transportShow = false;
-                $scope.practical_informationShow = false;
-
-            };
-
-            $scope.tab = 'home';
-            $scope.selectTab = function (setTab) {
-                if (setTab === 'home') {
+                if (data.history || data.overview || data.introductory) {
                     $scope.home = true;
-                    $scope.workShow = true;
-                    $scope.when_to_goShow = true;
-                    $scope.transportShow = true;
-                    $scope.practical_informationShow = true;
+                    $scope.homeMenu = true;
+                    if (_.get(data, 'history', null)) {
+                        $scope.histories = data.history;
+                        $scope.history = '';
+                        for (var i = 0; i < data.history.length; i++) {
+                            $scope.history += '\n' + data.history[i];
+
+                        }
+
+                        if (data.overview) {
+                            $scope.history += '\n' + _.get(data, 'overview', null);
+                        }
+
+                        if (data.overview) {
+                            $scope.history += '\n' + _.get(data, 'introductory.introduction.overview', null)
+                        }
+
+                    } else if (data.overview) {
+
+                        $scope.history += '\n' + data.overview
+                    } else if (data.introductory) {
+
+                        $scope.history += '\n' + _.get(data, 'introductory.introduction.overview', null);
+                    }
 
                 }
-                if (setTab === 'practical_information') {
-                    getSectionOff();
-                    $scope.practical_informationShow = true;
 
+
+                var result = [];
+                for (var key in data) {
+                    if (key == 'practical_information') {
+
+                        $scope.before_you_go = _.get(data, 'practical_information.health_and_safety.before_you_go', null);
+                        $scope.in_transit = _.get(data, 'practical_information.health_and_safety.in_transit', null);
+                        $scope.dangers_and_annoyances = _.get(data, 'practical_information.health_and_safety.dangers_and_annoyances', null);
+                        $scope.visas = _.get(data, 'practical_information.visas', null);
+                        $scope.while_youre_there = _.get(data, 'practical_information.health_and_safety.while_youre_there', null);
+
+
+                        $scope.cost = _.get(data, 'practical_information.money_and_costs.cost', null);
+                        $scope.money = _.get(data, 'practical_information.money_and_costs.money', null);
+                        if ($scope.cost || $scope.money) {
+                            $scope.money_and_costsShow = true;
+
+                        }
+                        if ($scope.before_you_go ||
+                            data.dangers_and_annoyances ||
+                            data.visas ||
+                            data.while_youre_there ||
+                            data.cost ||
+                            data.money) {
+                            $scope.practical_informationShow = true;
+                            $scope.practical_informationMenu = true;
+                        }
+
+
+                    }
+
+
+                    if (key == 'transport') {
+
+                        $scope.overviewTransport = _.get(data, 'transport.getting_around.overview', null);
+                        $scope.air = _.get(data, 'transport.getting_around.air', null);
+                        $scope.bicycle = _.get(data, 'transport.getting_around.bicycle', null);
+                        $scope.car_and_motorcycle = _.get(data, 'transport.getting_around.car_and_motorcycle', null);
+                        $scope.local_transport = _.get(data, 'transport.getting_around.local_transport', null);
+                        $scope.train = _.get(data, 'transport.getting_around.train', null);
+                        $scope.gettingThere = _.get(data, 'transport.getting_there_and_away.air', null);
+
+                        if ($scope.overviewTransport ||
+                            $scope.air ||
+                            $scope.bicycle ||
+                            $scope.car_and_motorcycle ||
+                            $scope.local_transport ||
+                            $scope.train ||
+                            $scope.gettingThere) {
+
+                            $scope.transportShow = true;
+                            $scope.transportMenu = true;
+                        }
+                    }
+
+
+                    if (key == 'weather') {
+                        if (data.weather.when_to_go) {
+                            $scope.overviewWeather = _.get(data, 'weather.when_to_go.overview', null);
+                            $scope.climate = _.get(data, 'weather.when_to_go.climate', null);
+                            if ($scope.overviewWeather ||
+                                $scope.climate) {
+                                $scope.when_to_goShow = true;
+                                $scope.when_to_goMenu = true;
+
+                            }
+                        }
+
+
+                    }
+                    if (key == 'work_live_study') {
+                        if (data.work_live_study.work) {
+
+                            $scope.overviewWork = _.get(data, 'work_live_study.work.overview', null);
+                            $scope.business = _.get(data, 'work_live_study.work.business', null);
+
+                            if ($scope.overviewWork ||
+                                $scope.business) {
+                                $scope.workShow = true;
+                                $scope.workMenu = true;
+
+                            }
+
+                        }
+
+
+                    }
                 }
-                if (setTab === 'transport') {
-                    getSectionOff();
-                    $scope.transportShow = true;
 
-                }
-                if (setTab === 'when_to_go') {
-                    getSectionOff();
-                    $scope.when_to_goShow = true;
 
-                }
-                if (setTab === 'work_live_study') {
-                    getSectionOff();
-                    $scope.workShow = true;
+                $scope.keys = result;
 
-                }
-                $scope.tab = setTab;
-                console.log($scope.tab);
-            };
 
-            $scope.isSelected = function (checkTab) {
-                return $scope.tab === checkTab;
-            };
+                $scope.limit = 900;
+                $scope.lessText = "Read less";
+                $scope.moreText = "Read more";
+                $scope.dotsClass = "toggle-dots-grey";
+                $scope.linkClass = "toggle-link-yellow";
 
-            console.log(data);
-        }).error(function (err) {
-            $scope.error = err;
-        });
+                var getSectionOff = function () {
+                    $scope.home = false;
+                    $scope.workShow = false;
+                    $scope.when_to_goShow = false;
+                    $scope.transportShow = false;
+                    $scope.practical_informationShow = false;
 
+                };
+
+                $scope.tab = 'home';
+                $scope.selectTab = function (setTab) {
+                    if (setTab === 'home') {
+                        $scope.home = true;
+                        $scope.workShow = true;
+                        $scope.when_to_goShow = true;
+                        $scope.transportShow = true;
+                        $scope.practical_informationShow = true;
+
+                    }
+                    if (setTab === 'practical_information') {
+                        getSectionOff();
+                        $scope.practical_informationShow = true;
+
+                    }
+                    if (setTab === 'transport') {
+                        getSectionOff();
+                        $scope.transportShow = true;
+
+                    }
+                    if (setTab === 'when_to_go') {
+                        getSectionOff();
+                        $scope.when_to_goShow = true;
+
+                    }
+                    if (setTab === 'work_live_study') {
+                        getSectionOff();
+                        $scope.workShow = true;
+
+                    }
+                    $scope.tab = setTab;
+                    console.log($scope.tab);
+                };
+
+                $scope.isSelected = function (checkTab) {
+                    return $scope.tab === checkTab;
+                };
+
+                console.log(data);
+            }).error(function (err) {
+                $scope.error = err;
+            });
+
+        })
     })
 
     .run(
